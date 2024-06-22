803f6124dc278b606616c947803850cf6493b0a8
Vlad <vchuev24@gmail.com>
2024-06-22 19:43:01 +0300

    Implement detailed page_parser

--- page_parser.py
+++ page_parser.py
@@ -20,9 +20,18 @@ def get_description(href: str):
         description = dict()
         for i in range(0, len(children), 2):  
             key = children[i].text.strip()
-            value = children[i + 1].text.strip()
+
+            value_elem = children[i + 1]
+            value_items = value_elem.find_elements(By.CSS_SELECTOR, "li")
+            value = [li_elem.text.strip().replace("-  ", "") for li_elem in value_items]
             description[key] = value
+        print(description)
         return description
     except Exception as e:
         print(f"Error getting description: {e}")
         return None
+    
+    
+if __name__ == "__main__":
+    get_description("https://www.loc.gov/item/2021670601/")
+    
