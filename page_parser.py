b1bde1f3e6d5e61bd04c70ffc1d5ca89d7efcf22
Vlad <vchuev24@gmail.com>
2024-06-26 16:58:26 +0300

    Refactor rewrite parser to class

--- page_parser.py
+++ page_parser.py
@@ -1,37 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
 
 
-def get_description(href: str):
-    driver.get(href)
-    try:
-        element = WebDriverWait(driver, 3).until(
+class PageParser:
+    url = "https://www.loc.gov/item"
+
+    def __init__(self, id: int) -> None:
+        self.id = id
+        href = f"{self.url}/{self.id}"
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(5)
+        driver.get(href)
+        self.driver = driver
+        WebDriverWait(driver, 3).until(
             EC.presence_of_element_located((By.ID, "item-cataloged-data"))
         )
-        children = driver.find_elements(By.CSS_SELECTOR, "#item-cataloged-data > *")
-        description = dict()
-        for i in range(0, len(children), 2):  
-            key = children[i].text.strip()
 
-            value_elem = children[i + 1]
-            value_items = value_elem.find_elements(By.CSS_SELECTOR, "li")
-            value = [li_elem.text.strip().replace("-  ", "") for li_elem in value_items]
-            description[key] = value
-        print(description)
-        return description
-    except Exception as e:
-        print(f"Error getting description: {e}")
-        return None
-    
-    
+    def get_description(self):
+
+        try:
+            children = self.driver.find_elements(
+                By.CSS_SELECTOR, "#item-cataloged-data > *"
+            )
+            description = dict()
+            for i in range(0, len(children), 2):
+                key = children[i].text.strip()
+
+                value_elem = children[i + 1]
+                value_items = value_elem.find_elements(By.CSS_SELECTOR, "li")
+                value = [
+                    li_elem.text.strip().replace("-  ", "") for li_elem in value_items
+                ]
+                description[key] = value
+            return description
+        except Exception as e:
+            print(f"Error getting description: {e}")
+            return None
+
+
 if __name__ == "__main__":
-    get_description("https://www.loc.gov/item/2021670601/")
     
+    parser = PageParser(id=2021670601)
+    print(parser.get_description())
